19fa4962f964df22c56b9a24c56510c2a607ff9f
Brett Zamir <brettz9@yahoo.com>
2019-12-27 17:23:49 +0800

    - Testing: Binary (unfinished)

---
+++ test/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {promisify} = require('util');
+const {join} = require('path');
+const {execFile: ef} = require('child_process');
+
+const execFile = promisify(ef);
+
+const binFile = join(__dirname, '../bin/index.js');
+
+describe('Binary', function () {
+  it('should return help', async function () {
+    this.timeout(5000);
+    const {stdout} = await execFile(binFile, ['-h']);
+    expect(stdout).to.contain(
+      "Builds a badge indicating your project's license(s)"
+    );
+  });
+
+  it('should return execute', async function () {
+    this.timeout(5000);
+    const {stdout, stderr} = await execFile(
+      binFile,
+      ['-l', 'test/fixtures/licenseInfo.json', 'test.svg']
+    );
+    console.log('stderr', stderr);
+    expect(stderr).to.contain('');
+  });
+});
